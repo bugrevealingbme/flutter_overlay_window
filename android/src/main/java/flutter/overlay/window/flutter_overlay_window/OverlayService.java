31a62b190e2eb1f2f7eb828c72190052fcbdf6ae
Burak Mete Erdoğan <bugrevealingbme@gmail.com>
2024-12-08 15:54:28 +0300

    Update OverlayService.java

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
@@ -164,8 +164,7 @@ public class OverlayService extends Service implements View.OnTouchListener {
                 WindowSetup.flag | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
                         | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
                         | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR
-                        | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
-                        | WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
+                        | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
                 PixelFormat.TRANSLUCENT
         );
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && WindowSetup.flag == clickableFlag) {

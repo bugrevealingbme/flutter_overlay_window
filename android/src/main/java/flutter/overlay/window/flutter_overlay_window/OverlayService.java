9a1d45658efa55128cd681990b52dc35d2e1816f
Burak Mete Erdoğan <bugrevealingbme@gmail.com>
2024-12-08 15:32:05 +0300

    Update OverlayService.java

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
@@ -164,18 +164,14 @@ public class OverlayService extends Service implements View.OnTouchListener {
                 WindowSetup.flag | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
                         | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
                         | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR
-                        | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
-                        | WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
                 PixelFormat.TRANSLUCENT
         );
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && WindowSetup.flag == clickableFlag) {
             params.alpha = MAXIMUM_OPACITY_ALLOWED_FOR_S_AND_HIGHER;
         }
-
-        params.gravity = Gravity.TOP | Gravity.LEFT;
-        params.x = 0;
-        params.y = -statusBarHeightPx(); // Durum çubuğu üstüne yerleşim
-  
+        params.gravity = WindowSetup.gravity;
         flutterView.setOnTouchListener(this);
         windowManager.addView(flutterView, params);
         moveOverlay(dx, dy, null);

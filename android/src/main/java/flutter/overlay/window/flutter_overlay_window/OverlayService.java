82b7f3350eae2ab1fe4f11236d7b74b3e32fa2e0
Burak Mete Erdoğan <bugrevealingbme@gmail.com>
2024-12-15 00:33:18 +0300

    Update OverlayService.java

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
@@ -118,13 +118,8 @@ public class OverlayService extends AccessibilityService implements View.OnTouch
     public static void removeOverlay() {
         if (instance != null && instance.windowManager != null) {
             instance.windowManager.removeView(instance.flutterView);
-            instance.windowManager = null;
-            instance.flutterView.detachFromFlutterEngine();
-            instance.flutterView = null;
-            instance.stopSelf();
+            isRunning = false;
         }
-        
-        isRunning = false;
     
         if (instance != null) {
             NotificationManager notificationManager = (NotificationManager) instance.getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);

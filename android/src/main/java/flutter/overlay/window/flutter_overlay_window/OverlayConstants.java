6efbc512c500ad3cfd2628992bec5cacef90118d
Burak Mete Erdoğan <bugrevealingbme@gmail.com>
2025-02-19 15:45:20 +0300

    Update OverlayConstants.java

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayConstants.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayConstants.java
@@ -9,4 +9,14 @@ final public class OverlayConstants {
     static final String CHANNEL_ID = "Overlay Channel";
     static final int NOTIFICATION_ID = 4579;
     static final int DEFAULT_XY = -6;
+
+    public static String ACCESSIBILITY_INTENT = "accessibility_event";
+    public static final String INTENT_GLOBAL_ACTION = "performGlobal";
+    public static final String INTENT_GLOBAL_ACTION_ID = "actionId";
+    public static final String INTENT_SYSTEM_GLOBAL_ACTIONS = "globalSystemAction";
+    public static final String BROD_SYSTEM_GLOBAL_ACTIONS = "globalSystemActionBrod";
+    public static final String SEND_BROADCAST = "SEND_BROADCAST";
+    public static final String ACCESSIBILITY_NODE = "ACCESSIBILITY_NODE";
+    public static final String SHARED_PREFS_TAG = "SHARED_PREFS_TAG";
+
 }
